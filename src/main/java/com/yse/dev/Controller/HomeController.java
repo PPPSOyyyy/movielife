cfd213a67a2cc1a2da96f07fe7ec69dae4418175
dw <mjh8394@gamil.com>
2026-09-02 11:06:12 +0900

    메인 페이지 및 화면 이동 수정

--- src/main/java/com/yse/dev/Controller/HomeController.java
+++ src/main/java/com/yse/dev/Controller/HomeController.java
@@ -3,21 +3,48 @@ package com.yse.dev.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class HomeController {
 
+
+    // ==========================================
+    // 메인 화면
+    // ==========================================
+    @GetMapping("/")
+    public String indexPage() {
+
+        return "index";
+    }
+
+
+    // ==========================================
+    // 회원가입 화면
+    // ==========================================
     @GetMapping("/signup")
     public String signupPage() {
+
         return "signup";
     }
 
+
+    // ==========================================
+    // 로그인 화면
+    // ==========================================
     @GetMapping("/login")
     public String loginPage() {
+
         return "login";
     }
 
+
+    // ==========================================
+    // 마이페이지 화면
+    // ==========================================
     @GetMapping("/mypage")
-    public String mypage() {
+    public String mypagePage() {
+
         return "mypage";
     }
+
 }
